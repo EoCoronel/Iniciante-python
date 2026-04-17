0135c00d8d573e1c812df5be43d2ea9257725a02
EoCoronel <samuelfernandes201324@gmail.com>
2026-04-17 10:27:46 -0300

    Implement player registration and search functionality
    The code allows for input of player names and positions, and includes multiple search queries for player information.

---
+++ Bancodedados1.py
@@ -0,0 +1,48 @@
+time = []
+
+# Cadastro
+for i in range(7):
+    nome = input ("Digite seu nome: ")
+    posição = input("Digite sua posição: ")
+    
+    time.append([nome, posição])
+    
+# Consulta
+busca = input("\nDigite o nome para buscar: ")
+
+for jogador in time:
+    if jogador[0] == busca:
+        print("Jogador:", jogador [0])
+        print("Posição:", jogador [1])
+
+# Consulta
+busca = input("\nDigite o nome para buscar: ")
+
+for jogador in time:
+    if jogador[0] == busca:
+        print("Jogador:", jogador [0])
+        print("Posição:", jogador [1])
+
+# Consulta
+busca = input("\nDigite o nome para buscar: ")
+
+for jogador in time:
+    if jogador[0] == busca:
+        print("Jogador:", jogador [0])
+        print("Posição:", jogador [1])
+
+# Consulta
+busca = input("\nDigite o nome para buscar: ")
+
+for jogador in time:
+    if jogador[0] == busca:
+        print("Jogador:", jogador [0])
+        print("Posição:", jogador [1])
+
+# Consulta
+busca = input("\nDigite o nome para buscar: ")
+
+for jogador in time:
+    if jogador[0] == busca:
+        print("Jogador:", jogador [0])
+        print("Posição:", jogador [1])
