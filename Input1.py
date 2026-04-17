c32f35906bd716a14c5d844ed201099a93f7f5b9
EoCoronel <samuelfernandes201324@gmail.com>
2026-04-16 23:46:04 -0300

    Add interactive greeting script in Input1.py

---
+++ Input1.py
@@ -0,0 +1,15 @@
+nome = input("Digite seu nome: ")
+
+print("Ola,", nome)
+print("Seja bem vindo")
+print("Para nos conhecermos melhor, nos diga sua idade!")
+
+idade = input("Qual é a sua idade? ")
+
+print("Que legal! ", nome, "Agora que sei sua idade, me diga seus hobbies favoritos!")
+
+hobbies = input("Quais são seus hobbies? ")
+
+print("Que hobbies divertidos,", nome, "!")
+print("Foi ótimo te conhecer,", nome, "agora tenho que ir")
+print("Tchau, até uma próxima, meu colega!")
