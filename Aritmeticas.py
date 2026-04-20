71e776eb3c8bd3b7e646c30b2fe88589ef45d7ff
EoCoronel <samuelfernandes201324@gmail.com>
2026-04-20 00:55:18 -0300

    Add arithmetic average calculation in Aritmeticas.py

---
+++ Aritmeticas.py
@@ -0,0 +1,7 @@
+a = float(input("Digite o primeiro número: "))
+b = float(input("Digite o segundo número: "))
+c = float(input("Digite o terceiro número: "))
+
+média = (a + b + c) / 3
+
+print(f"A média calculada é: {média:.2f}")
